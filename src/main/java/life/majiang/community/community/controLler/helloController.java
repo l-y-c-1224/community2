9cd62a96d171b45d4ec25f3646361cc14d7524b1
l-y-c-1224 <17610750093@163.com>
2019-11-18 04:34:46 +0800

    git init

---
+++ src/main/java/life/majiang/community/community/controLler/helloController.java
@@ -0,0 +1,21 @@
+package life.majiang.community.community.controLler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name ="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+
+    }
+}
+
+
